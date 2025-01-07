e08f065ec7565e277c7eee07f9567f791e5030c2
Ariful Islam Akash <ariful.akash23@gmail.com>
2025-01-07 17:41:28 +0600

    create

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Estore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult ProductDetails()
+        {
+            return View();
+        }
+        public IActionResult Cart()
+        {
+            return View();
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
