7261f0e5687a66e6385bc643d8cf860811bd100c
Ariful Islam Akash <ariful.akash23@gmail.com>
2025-01-07 19:06:11 +0600

    create lookup.cs

---
+++ Datamodel/Lookup.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Estore.Datamodel
+{
+    public class Lookup
+    {
+        [Key]
+        public int LookupId { get; set; }
+        public string? LookupType { get; set; }
+        public string? LookupData { get; set; }
+        public string? LookupIcon { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
